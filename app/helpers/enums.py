cc63f6854aa1cd173301b7f9a4fd2a6112b3e58b
vgnam <vgnam112358@gmail.com>
2025-12-03 22:19:49 +0700

    remove unused file inside schema and helpers package

--- app/helpers/enums.py
+++
@@ -1,5 +0,0 @@
-import enum
-
-class UserRole(enum.Enum):
-    ADMIN = "admin"
-    GUEST = "guest"
--- tests/faker/__init__.py
+++
@@ -1,7 +0,0 @@
-import faker.providers
-
-from tests.faker.user_provider import UserProvider
-
-fake = faker.Faker()
-
-fake.add_provider(UserProvider)
